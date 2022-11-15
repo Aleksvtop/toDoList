550d90517e271bafaf0b992d1ae4ff22b3f62b96
Aleksvtop <vazilino@gmail.com>
2022-11-15 14:14:09 +0300

    Add hook useState for removing tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 
 function App() {
 
-    let tasks = [
+    let [tasks, setTasks] = useState([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
         {id: 4, title: "React API", isDone: false},
         {id: 5, title: "GraphQL", isDone: false}
-    ]
+    ])
 
     function removeTask(id: number) {
-        tasks = tasks.filter(task => task.id !== id)
-        console.log(tasks)
+        let filteredTasks = tasks.filter(task => task.id !== id)
+        setTasks(filteredTasks)
     }
 
     return (
