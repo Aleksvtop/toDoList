bf0e9b6eba6d213598d0d5d50c4a99b1de595007
Aleksvtop <vazilino@gmail.com>
2023-02-10 11:18:57 +0300

    Create SuperInput component

---
+++ src/components/SuperInput.tsx
@@ -0,0 +1,42 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+
+type PropsSuperInputType={
+    callBack: (title:string) => void
+}
+
+export const SuperInput = (props:PropsSuperInputType) => {
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+    const addTask = () => {
+        let newTitle = title.trim()
+        if (newTitle !== "") {
+            props.callBack(newTitle);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.key === 'Enter') {
+            addTask();
+        }
+    }
+    return (
+        <div>
+            <input value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   className={error ? "error" : ""}
+            />
+            <button onClick={addTask}>+</button>
+            {error && <div className="error-message">{error}</div>}
+        </div>
+    );
+};
+
+export default SuperInput;
